e356685f32710dc649a772494bb5b1fe2c48b548
Jiwon Bae <jwb8705@gmail.com>
2018-07-21 12:52:03 +0900

    chore: [master] - Creates projects

---
+++ src/main/java/com/teamproject/drinkit/DrinkitApplication.java
@@ -0,0 +1,12 @@
+package com.teamproject.drinkit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrinkitApplication.class, args);
+    }
+}
