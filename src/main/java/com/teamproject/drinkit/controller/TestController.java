652b04c4d047a484e577873d42934da1ed577200
Jiwon Bae <jwb8705@gmail.com>
2018-07-25 23:27:41 +0900

    feat: [master] - Creates test controller and html page to test if auto deploy successes

---
+++ src/main/java/com/teamproject/drinkit/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.teamproject.drinkit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+    @GetMapping("")
+    public String index(){
+        return "index";
+    }
+}
