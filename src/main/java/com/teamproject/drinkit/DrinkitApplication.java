85562b0a9b4390990641dc57d350a2dd08ab2c9d
Jiwon Bae <jwb8705@gmail.com>
2018-08-12 14:30:25 +0900

    chore: [project] - Adds profile about production server to test if security works fine

--- src/main/java/com/teamproject/drinkit/DrinkitApplication.java
+++ src/main/java/com/teamproject/drinkit/DrinkitApplication.java
@@ -2,11 +2,19 @@ package com.teamproject.drinkit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class DrinkitApplication {
+    private static final String APPLICATION_LOCATIONS = "spring.config.location="
+            + "classpath:application.yml,"
+            + "/app/config/drinkit/real-application.yml";
 
     public static void main(String[] args) {
-        SpringApplication.run(DrinkitApplication.class, args);
+
+        new SpringApplicationBuilder(DrinkitApplication.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 }
