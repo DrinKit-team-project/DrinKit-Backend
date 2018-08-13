065ff1cc541d0f370281068ea6f1116c922f310e
Jiwon Bae <jwb8705@gmail.com>
2018-08-14 00:25:18 +0900

    feat: Removes datasource auto config exclude option

--- src/main/java/com/teamproject/drinkit/DrinkitApplication.java
+++ src/main/java/com/teamproject/drinkit/DrinkitApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class DrinkitApplication {
     private static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
