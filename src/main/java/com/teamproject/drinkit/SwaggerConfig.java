3583e5be1f9294538ac1b58c034a21e89dd8ae85
Jiwon Bae <jwb8705@gmail.com>
2018-08-14 22:22:30 +0900

    chore: [deploy-test] - Fix code to see log

--- src/main/java/com/teamproject/drinkit/SwaggerConfig.java
+++ src/main/java/com/teamproject/drinkit/SwaggerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+
 public class SwaggerConfig {
 
 }
