48fbad48a8a58d073e75f3511112be118f7b3471
Jiwon Bae <jwb8705@gmail.com>
2018-08-14 13:36:57 +0900

    feat: [deploy-test] - Makes token related test pass

--- src/test/java/com/teamproject/drinkit/service/FacebookInfoFetchServiceImpTest.java
+++ src/test/java/com/teamproject/drinkit/service/FacebookInfoFetchServiceImpTest.java
@@ -18,7 +18,7 @@ public class FacebookInfoFetchServiceImpTest {
 
     @Before
     public void setUp() throws Exception {
-        String access_token = "EAAM7HscfrTwBAEUSbxUGFIfuntfiyMAacKf57ZCfZBSkZBOZAvoEz378MIKyGPFPBEdP7JKiVdS0MbZCcZATx0DrCqgmljJBpH7NUiE5o86bqjsyihiQIY5ZAhetjG69aZAqB1HQ49FvZCX39hZCuFD3j0PygtiBcLh3kaVopmePRcvwZBe9NOmh6ZAAXOw3gtIcuZCi5zVdlgD3CgwZDZD";
+        String access_token = "EAAM7HscfrTwBAPXCNKCQDorGdbt2ZCGLGqBgpPzZBFupWTHMZCd8ZClRvdocn2m61zqp776OxNFnm0ZBYne8pQ7pnFwfZB1sz8ue8BtYrmmpZBAtasibZAAQ3r9bWW4JiDujZBDtKyF3sQ6vTcE8ZCbJDE2GfKYhgXZA5i7pd9TvVW7xBKJG2yApc7i4lpAstPO3mvoO0J3lUA1ngZDZD";
         String user_id = "704798269891295";
         facebookInfoFetchServiceImp = new FacebookInfoFetchServiceImp();
         dto = new SocialLoginDto(SocialProviders.FACEBOOK, access_token, user_id);
