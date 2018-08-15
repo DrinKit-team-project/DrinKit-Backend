fe5a637024389c2b1bf1a8cb83305ab9e29292be
Jiwon Bae <jwb8705@gmail.com>
2018-08-15 20:18:31 +0900

    feat: [swagger2] - Makes connection with Swagger2

--- src/main/java/com/teamproject/drinkit/WebMVCConfig.java
+++ src/main/java/com/teamproject/drinkit/WebMVCConfig.java
@@ -1,9 +1,25 @@
 package com.teamproject.drinkit;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebMVCConfig {
+@EnableWebMvc
+@EnableAspectJAutoProxy
+@Component("controller")
+public class WebMVCConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 
 }
