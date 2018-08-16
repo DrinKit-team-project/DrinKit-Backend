4a6284d53d0094623b97e6e7feee54e245d7c3ca
KOOSANGYOON <nevergiveup936@naver.com>
2018-08-16 14:10:24 +0900

    feat: [menu-management] - Edit test code.

--- src/test/java/com/teamproject/drinkit/controller/ApiCafeControllerTest.java
+++ src/test/java/com/teamproject/drinkit/controller/ApiCafeControllerTest.java
@@ -4,15 +4,25 @@ import com.teamproject.drinkit.domain.Cafe;
 import com.teamproject.drinkit.domain.CafeRepository;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ApiCafeControllerTest {
+
+    private static final Logger log = LoggerFactory.getLogger(ApiCafeControllerTest.class);
+
     @Autowired
     private CafeRepository cafeRepository;
 
@@ -32,6 +42,6 @@ public class ApiCafeControllerTest {
     @Test
     public void main() {
         ResponseEntity<String> response = template.getForEntity("/api/cafes",  String.class);
-        assertThat(response.getStatusCode(), is(HttpStatus.OK));
+        assertThat(response.getStatusCode(), is(HttpStatus.FOUND));
     }
 }
