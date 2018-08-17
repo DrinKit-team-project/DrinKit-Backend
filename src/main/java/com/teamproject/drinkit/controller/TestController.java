7c5299522699f4975fea52609758fd16ca36b126
Jiwon Bae <jwb8705@gmail.com>
2018-08-17 16:22:47 +0900

    feat: [deploy-test] - Tests deployment

--- src/main/java/com/teamproject/drinkit/controller/TestController.java
+++ src/main/java/com/teamproject/drinkit/controller/TestController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String index(){
-        return "index";
+        return "hello";
     }
 }
