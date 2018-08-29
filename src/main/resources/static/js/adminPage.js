16e93264c91c16ed1a34e59b8f54c7f4a810d91a
KOOSANGYOON <nevergiveup936@naver.com>
2018-08-29 15:00:13 +0900

    feat: [feature/nogada] - Make admin page. this is only add cafe. menu is not ready.

--- src/main/resources/static/js/adminPage.js
+++ src/main/resources/static/js/adminPage.js
@@ -1,7 +1,6 @@
 'use strict';
 
 $(".cafe-register-button").on("click", registerCafe);
-$(".addCategoryBtn").on("click", addCategoryInCafe);
 
 function registerCafe(e) {
     e.preventDefault();
@@ -21,19 +20,9 @@ function registerCafe(e) {
         data: nameAndUrl,
         dataType: 'json'}).done(function makeCafeSuccess(data) {
             console.log("success to make cafe.");
-            // window.location.reload();
+            window.location.reload();
     }).fail(function makeCafeFail(data) {
         console.log("fail...");
         alert("fail to make cafe.");
     });
-}
-
-function addCategoryInCafe(e) {
-    e.preventDefault();
-    console.log("hihihi");
-
-    var categoryName = $("#cafe-category").val();
-    // var categoryName = $(e.target).closest(".add-text-area").val();
-    var url = $(e.target).val();
-    console.log("name : ", categoryName + " | url : ", url);
 }
