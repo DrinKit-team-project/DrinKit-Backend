991776f6666a738d78322fe0080bf1131200fc4a
Jiwon Bae <jwb8705@gmail.com>
2018-09-06 18:47:47 +0900

    feat: [s3-uploader] - Adds Spring cloud AWS dependecies and creates S3Uploader component

---
+++ src/main/java/com/teamproject/drinkit/component/S3Uploader.java
@@ -0,0 +1,8 @@
+package com.teamproject.drinkit.component;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class S3Uploader {
+    private AmazonS3Client amazonS3Client;
+}
