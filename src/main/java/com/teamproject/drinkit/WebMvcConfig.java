ed3cf4d6bae00541af716961ffd4e88932ff87a5
Jiwon Bae <jwb8705@gmail.com>
2018-09-13 18:13:58 +0900

    feat: [master] - Adds filter and HttpMessageConverter bean to support utf-8 encoding

---
+++ src/main/java/com/teamproject/drinkit/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.teamproject.drinkit;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.Filter;
+import java.nio.charset.Charset;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    public HttpMessageConverter<String> responseBodyConverter(){
+        return new StringHttpMessageConverter(Charset.forName("UTF-8"));
+    }
+
+    @Bean
+    public Filter characterEncodingFilter(){
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return characterEncodingFilter;
+    }
+}
