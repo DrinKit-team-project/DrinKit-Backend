47dcba5ca7c09e8e9fee5e6fbdc03be660500a98
Jiwon Bae <jwb8705@gmail.com>
2018-09-14 12:33:19 +0900

    feat: [master] - Redefines character encoding filter to resolve utf8 encoding problem

--- src/main/java/com/teamproject/drinkit/WebMvcConfig.java
+++ src/main/java/com/teamproject/drinkit/WebMvcConfig.java
@@ -3,24 +3,21 @@ package com.teamproject.drinkit;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
 
 @Configuration
 @EnableWebMvc
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    @Order(Ordered.HIGHEST_PRECEDENCE)
     @Bean
-    public FilterRegistrationBean<CharacterEncodingFilter> registerCharacterEncodingFilterBean() throws Exception {
-        FilterRegistrationBean<CharacterEncodingFilter> filterRegistrationBean = new FilterRegistrationBean<>(characterEncodingFilter());
-        filterRegistrationBean.setName("CharacterEncodingFilter");
-        filterRegistrationBean.addUrlPatterns("/api/**");
-        filterRegistrationBean.setOrder(1);
-        return filterRegistrationBean;
-    }
-
-    private CharacterEncodingFilter characterEncodingFilter() throws  Exception{
+    public Filter characterEncodingFilter() {
         CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
         characterEncodingFilter.setEncoding("UTF-8");
         characterEncodingFilter.setForceEncoding(true);
