0069af690d6518ce0f26a9e31cd64f5a88415031
Jiwon Bae <jwb8705@gmail.com>
2018-10-04 17:36:37 +0900

    feat: [master] - Fixes error

--- src/main/java/com/teamproject/drinkit/support/S3Uploader.java
+++ src/main/java/com/teamproject/drinkit/support/S3Uploader.java
@@ -3,9 +3,9 @@ package com.teamproject.drinkit.support;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -18,13 +18,9 @@ import java.util.Optional;
 @Component
 public class S3Uploader {
     private static final Logger log = LoggerFactory.getLogger(S3Uploader.class);
-    
-    private final AmazonS3Client amazonS3Client;
 
-
-    public S3Uploader(AmazonS3Client amazonS3Client) {
-        this.amazonS3Client = amazonS3Client;
-    }
+    @Autowired
+    private AmazonS3Client amazonS3Client;
 
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
@@ -40,6 +36,7 @@ public class S3Uploader {
         removeNewFile(uploadFile);
         return uploadImageUrl;
     }
+
     private String putS3(File uploadFile, String fileName){
         amazonS3Client.putObject(new PutObjectRequest(bucket, fileName, uploadFile).withCannedAcl(CannedAccessControlList.PublicRead));
         return amazonS3Client.getUrl(bucket,fileName).toString();
