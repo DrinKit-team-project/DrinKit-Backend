8c49ebfdb6feffdd55f6d1386ea3d0555a14dcba
Jiwon Bae <jwb8705@gmail.com>
2018-10-04 17:29:22 +0900

    feat: [master] - Fixes error

--- src/main/java/com/teamproject/drinkit/support/S3Uploader.java
+++ src/main/java/com/teamproject/drinkit/support/S3Uploader.java
@@ -4,6 +4,8 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,9 +15,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Optional;
 
-@Slf4j
 @Component
 public class S3Uploader {
+    private static final Logger log = LoggerFactory.getLogger(S3Uploader.class);
+    
     private final AmazonS3Client amazonS3Client;
 
 
